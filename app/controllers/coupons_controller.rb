ac548677b9d949aabe050cdfb635af86943daec6
Ranika Williams <rnkprice07@gmail.com>
2019-10-21 18:53:47 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -19,6 +19,7 @@ class CouponsController < ApplicationController
     
     
     
-    
+     @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+    redirect_to coupon_path(@coupon)
   end
 end
